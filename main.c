40a2d51aad293c58aaec1e681f21eceaf11ef0d4
pcodefromvoidl <primecoder8@gmail.com>
2026-03-13 17:16:42 +0530

    add support for and

--- main.c
+++ main.c
@@ -143,10 +143,23 @@ int main(int argc, const char *argv[])
             update_flage(r0);
             break;
         case OP_AND:
-            unimplemented();
+            uint16_t r0 = (instr >> 9) & 0x7;
+            uint16_t r1 = (instr >> 6) & 0x7;
+            uint16_t imm_flag = (instr >> 5) & 0x1;
+            if (imm_flag)
+            {
+                uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                reg[r0] = reg[r1] & imm5; 
+            }
+            else 
+            {
+                uint16_t r2 = instr & 0x7;
+                reg[r0] = reg[r1] & reg[r2];   
+            }
+            update_flage(r0);    
             break;
         case OP_NOT:
-            unimplemented();
+            
             break;
         case OP_BR:
             unimplemented();
